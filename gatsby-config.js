1e994094a6a91e6039ff8d1dbd2d6cbebaa19e5f
Kaushani <kushi1359@gmail.com>
2018-09-16 13:20:29 +0530

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,73 @@
+module.exports = {
+  siteMetadata: {
+    title: "Voracious",
+    author: "Mohit Vora",
+    homeCity: "Bengaluru",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
+        ],
+      },
+    },
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-774017-3`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Bricolage",
+        short_name: "Bricolage",
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "1024x1024",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        background_color: "white",
+        theme_color: "white",
+        display: "minimal-ui",
+      },
+    },
+    `gatsby-plugin-offline`,
+    // `gatsby-plugin-preact`,
+    `gatsby-plugin-react-helmet`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,89 @@
+import React from "react"
+import Helmet from "react-helmet"
+import Link from "gatsby-link"
+
+import Layout from "../layouts/index.js"
+import typography from "../utils/typography"
+import profilePic from "../images/kyle-round-small-pantheon.jpg"
+
+const rhythm = typography.rhythm
+
+class BlogIndexRoute extends React.Component {
+  render() {
+    // console.log(this.props)
+    const posts = this.props.data.allMarkdownRemark.edges
+    const siteTitle = this.props.data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location}>
+        <Helmet title={siteTitle} />
+        <p
+          style={{
+            marginBottom: rhythm(1.5),
+          }}
+        >
+          <img
+            src={profilePic}
+            style={{
+              borderRadius: `100%`,
+              float: "left",
+              marginRight: rhythm(1 / 4),
+              marginBottom: 0,
+              width: rhythm(2),
+              height: rhythm(2),
+            }}
+          />
+          Written by <strong>{this.props.data.site.siteMetadata.author}</strong>{" "}
+        </p>
+        <ul
+          style={{
+            marginBottom: 0,
+          }}
+        >
+          {posts.map(post => (
+            <li key={post.node.fields.slug}>
+              <Link
+                style={{
+                  textDecoration: "none",
+                }}
+                to={post.node.fields.slug}
+              >
+                {post.node.frontmatter.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndexRoute
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+        author
+        homeCity
+      }
+    }
+    allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { draft: { ne: true } } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
